2f4043402840b6af4e171332cd53d0242a1b0c87
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-07-31 17:06:59 +0200

    sidebar in sidebar.html statt index.html

---
+++ js/contacts.js
@@ -0,0 +1,12 @@
+function init(){
+    renderMainDivs();
+}
+
+function renderMainDivs(){
+    let content = document.getElementById('content');
+    console.log('funktioniert');
+    content.innerHTML='';
+    content.innerHTML=`
+    <div class="contacts-list" id="contactsList"></div>
+    <div class="contact-details" id="contactDetails"></div`
+}

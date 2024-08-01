6fd9d680addabb7cdb2d0d5a607142273588852a
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-08-01 07:38:54 +0200

    Funktionsbeschreibungen

--- js/contacts.js
+++ js/contacts.js
@@ -1,8 +1,14 @@
+/**
+ * Diese Funktion dient dazu alle Funktionen, die für das Rendern verantwortlich sind, nach dem Laden der Seite zu rendern
+ */
 function initContacts(){
     renderMainContacts();
     renderContacts();
 }
 
+/**
+ * Diese Funktion soll die beiden Container für den Content erstellen.
+ */
 function renderMainContacts(){
     let content = document.getElementById('content');
     console.log('funktioniert');
@@ -12,6 +18,9 @@ function renderMainContacts(){
     <div class="contact-details" id="contactDetails"></div`
 }
 
+/**
+ * Diese Funktion soll den Button und den unteren Container für die Kontakte erstellen
+ */
 function renderContacts(){
     let contactsList = document.getElementById('contactsList');
     contactsList.innerHTML='';
@@ -23,6 +32,10 @@ function renderContacts(){
     <div class="contacts-list-bottom" id="contactsListBottom"></div>`
 }
 
+/**
+ * Diese Funktion soll das Overlay für Kontakte hinzufügen öffnen
+ */
 function openNewContactOverlay(){
     console.log('overlayFunction');
 }
+

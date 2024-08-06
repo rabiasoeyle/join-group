58006af087a775515047998ed479e5f418be710f
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-08-06 08:07:28 +0200

    Cod made more readable

--- firebase.js
+++ firebase.js
@@ -1,41 +1,41 @@
-const FIREBASE_URL = 'hier eure eigene URL zur database reinpacken';
+const FIREBASE_URL = "hier eure eigene URL zur database reinpacken";
 let users = [];
 
-async function loadUsers(path = '/names') {
-    let userResponse = await fetch(FIREBASE_URL + path + '.json');
-    let responseToJson = await userResponse.json();
-    console.log(responseToJson);
+async function loadUsers(path = "/names") {
+  let userResponse = await fetch(FIREBASE_URL + path + ".json");
+  let responseToJson = await userResponse.json();
+  console.log(responseToJson);
 
-    if (responseToJson) {
-        Object.keys(responseToJson).forEach(key => {
-            users.push({
-                id: key,
-                name: responseToJson[key]['name'],
-                phone: responseToJson[key]['phone']
-            })
-        });
-        console.log(users);
-    }
+  if (responseToJson) {
+    Object.keys(responseToJson).forEach((key) => {
+      users.push({
+        id: key,
+        name: responseToJson[key]["name"],
+        phone: responseToJson[key]["phone"],
+      });
+    });
+    console.log(users);
+  }
 }
 
 async function addUser() {
-    let nameValue = document.getElementById('name').value;
-    let phoneValue = document.getElementById('phone').value;
-    let newUser = { name: nameValue, phone: phoneValue };
-    document.getElementById('name').value = '';
-    document.getElementById('phone').value = '';
-    await postData('/names', newUser);
-    await loadUsers('/names');
+  let nameValue = document.getElementById("name").value;
+  let phoneValue = document.getElementById("phone").value;
+  let newUser = { name: nameValue, phone: phoneValue };
+  document.getElementById("name").value = "";
+  document.getElementById("phone").value = "";
+  await postData("/names", newUser);
+  await loadUsers("/names");
 }
 
-async function postData(path = '', data = {}) {
-    await fetch(FIREBASE_URL + path + '.json', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data)
-    });
+async function postData(path = "", data = {}) {
+  await fetch(FIREBASE_URL + path + ".json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 }
 
 // id = path in firebase

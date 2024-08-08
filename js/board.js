d56e97a8a494b6926d4747fc848979bb52ccf330
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-08-08 11:23:23 +0200

    grundlagen bei board werden angezeigt

--- js/board.js
+++ js/board.js
@@ -6,9 +6,11 @@ if(msg){
 let firebase_URL =
   "https://join-2-b992b-default-rtdb.europe-west1.firebasedatabase.app/";
 let tasks =[];
+let contacts=[];
 
 async function initBoard() {
-    await loadTasks()
+    await loadTasks();
+    await loadContacts();
     todoBoard();
     inProgressBoard();
     awaitFeedbackBoard();
@@ -21,16 +23,17 @@ function todoBoard(){
     for(i=0; i<tasks.length;i++){
       content.innerHTML +=`
       <div class="one-task-div">
-        <div>${tasks[i]['priority']}</div>
+        <div id="categorySign-${i}">${tasks[i]['category']}</div>
         <div>${tasks[i]['title']}</div>
         <div>${tasks[i]['description']}</div>
         <div>
             <div></div>
             <div> 0/0-Subtasks</div>
         </div>
-        <div id="assignedPerson-${i}"></div>
+        <div class="assigned-persons-initals"id="assignedPerson-${i}"></div>
       </div>`;
       showAssignedPersonsInitial(i);
+      categorySign(i);
     }
 }
 
@@ -40,11 +43,45 @@ function showAssignedPersonsInitial(i){
   assignedPersons = tasks[i]['assigned']
   for(j=0; j<assignedPersons.length; j++){
     persons.innerHTML +=`
-    <div>${assignedPersons[j]['name']}</div>
+    <div class="initals-div-in-task"style="background-color:${assignedPersons[j]['color']}">${profileInitials(assignedPersons[j]['name'])}</div>
     `;
   }
-
 }
+
+function categorySign(){
+  let category = document.getElementById(`categorySign-${i}`);
+  if(tasks[i]['category'] =='Technical Task'){
+    category.style.backgroundColor = '#1FD7C1';
+    category.style.color = 'white';
+  }else if(tasks[i]['category'] =='User Story'){
+    category.style.backgroundColor ='#0038FF';
+    category.style.color = 'white';
+  }
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';//jederBuchstabe des Farbstrings
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+/**
+ * In dieser Funktion werden die Initialien der Kontakte rausgefiltert und wiedergegeben
+ *
+ * @param {*} i
+ * @returns
+ */
+function profileInitials(name) {
+  let names = name.split(" "),
+    initials = names[0].substring(0, 1).toUpperCase();
+  if (names.length > 1) {
+    initials += names[names.length - 1].substring(0, 1).toUpperCase();
+  }
+  return initials;
+}
+
 function inProgressBoard(){
 
 }

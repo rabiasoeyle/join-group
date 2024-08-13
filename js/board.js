0021271a38cc879c96e2c2a3aec63de67d2a0039
rabiasoeyle <ing.rabia.soeylemez@gmail.com>
2024-08-13 08:49:45 +0200

    color funktion vom script.js genommen

--- js/board.js
+++ js/board.js
@@ -358,3 +358,16 @@ function highlight(id) {
 function removeHighlight(id) {
     document.getElementById(id).classList.remove('drag-area-highlight');
 }
+
+/**
+ * Diese Funktion sorgt dafür, dass eine zufällige Farbe erstellt wird
+ * @returns 
+ */
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';//jederBuchstabe des Farbstrings
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}

67f88a9f9585a98c39d4b9912bc39c14526d7015
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-08-19 13:42:06 +0200

    login test

--- js/login.js
+++ js/login.js
@@ -15,3 +15,45 @@ function singUp() {
   document.getElementById("blue_signed_up").classList.toggle("d-none");
   document.getElementById("help_initials_mobile").classList.toggle("d-none");
 }
+
+/**
+ * Diese Funktion dient dazu, die Werte aus den Inputfeldern für den neuen User auszulesen und sie an die postData() weiterzugeben.
+ */
+
+async function neuUser() {
+  let nameValue = document.getElementById("neuUserLoginName").value.trim();
+  let emailValue = document.getElementById("neuUserLoginEmail").value.trim();
+  let passwortValue = document.getElementById("neuUserLoginPasswort").value.trim();
+  let numberValue = "-";
+  let colorValue = getRandomColor();
+  let newContact = {name: nameValue, email: emailValue, passwort: passwortValue, phone: numberValue, color: colorValue,};
+  nameValue = "";
+  emailValue = "";
+  passwortValue = "";
+  contacts = [];
+  await postData("/contacts", newContact);
+  await loadContacts("/contacts");
+  document.getElementById("contactDetailsBottom").innerHTML = "";
+  console.log(contacts);
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF"; //jederBuchstabe des Farbstrings
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function login() {
+  let email = document.getElementById("loginEmail").value.trim();
+  let password = document.getElementById("loginPassword").value.trim();
+  let user = user.find(
+    (u) => u.email == email.value && u.password == password.value
+  );
+  console.log(user);
+  if (user) {
+    console.log("User gefunden");
+  }
+}
